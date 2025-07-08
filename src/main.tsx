b1e2b17d1a5f1664f2356a44b3d5201bf3d1b081
Mauro Vio <maurovio.dev@gmail.com>
2025-07-08 13:45:50 +0200

    perf(query): refactor app structure and integrate React Query
    Updated app entry point to use a Providers component for React Query integration. Relocated assets,
    adjusted references accordingly, and updated background color and dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { StrictMode } from "react";
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 // biome-ignore lint/style/noNonNullAssertion: The element with id 'root' is guaranteed to exist in the DOM
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
